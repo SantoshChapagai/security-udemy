66fe143359d0c0ef0ecb06eda1938e06c8a39b21
SantoshChapagai <santoshchapagai100@gmail.com>
2023-07-25 12:56:43 +0300

    successfully rendering another page after resgistration and successful login

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
+
+const userSchema = {
+  email: String,
+  password: String
+};
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function (req, res) {
   res.render("home");
@@ -20,6 +29,34 @@ app.get("/register", function (req, res) {
   res.render("register");
 });
 
+app.post("/register", function (req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  const saveUser = newUser.save();
+  if (saveUser) {
+    res.render("secrets");
+  } else {
+    res.render(err);
+  }
+
+});
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username })
+    .then((founderUser) => {
+      if (founderUser) {
+        if (founderUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    })
+    .catch((err) => res.send("Incorrect username or password" + err));
+});
 
 app.listen(3000, function () {
   console.log("server is running at port 3000");
